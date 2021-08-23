b74dfd58c8877307e11cc72e8e73f92b2065e68d
ifeanyi06 <ifeanyi06@hotmail.com>
2021-08-23 08:34:22 +0100

    Add project files.

---
+++ SnakesAndLadders/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLadders
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string GamesWon { get; set; }
+    }
+}
