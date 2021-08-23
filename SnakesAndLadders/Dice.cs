b047524bd0525fad4968ab363d723fffb55215c0
ifeanyi06 <ifeanyi06@hotmail.com>
2021-08-23 08:49:20 +0100

    Created dice with rolling capabilities

---
+++ SnakesAndLadders/Dice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLadders
+{
+    public class Dice
+    {
+        private readonly int numberOfDie;
+
+        public Dice(int numberOfDie)
+        {
+            this.numberOfDie = numberOfDie;
+        }
+        public int RollDice
+        {
+            get
+            {
+                Random ran = new Random();
+                return ran.Next(1, numberOfDie);
+            }
+        }
+    }
+}
--- SnakesAndLadders/Program.cs
+++ SnakesAndLadders/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SnakesAndLadders
 {
     class Program
     {
+        private static Dictionary<int, int> snakeAndLadderLinks = new Dictionary<int, int>() {
+            {4, 14},
+            {9, 31},
+            {17, 7},
+            {20, 38},
+            {28, 84},
+            {40, 59},
+            {51, 67},
+            {54, 34},
+            {62, 19},
+            {63, 81},
+            {64, 60},
+            {71, 91},
+            {87, 24},
+            {93, 73},
+            {95, 75},
+            {99, 78},
+        };
+
         static void Main(string[] args)
         {
+
+
             Console.WriteLine("Hello World!");
         }
     }
